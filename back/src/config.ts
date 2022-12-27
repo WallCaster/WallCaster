8bff6850dd96ec9cee538876305a532bf9a2133d
CodyAdam <cody.ad4@gmail.com>
2022-12-27 17:52:02 +0100

    feat: config to class +singleton

--- back/src/config.ts
+++ back/src/config.ts
@@ -1,61 +1,73 @@
 import { join } from 'path';
 import { readFileSync, writeFileSync } from 'fs';
 
-export type Config = {
-  numberOfScreens: number;
-  dataRange: DateRange;
-  forbiddenWords: string[];
-  whitelistAuthors: string[];
-  whitelistHashtags: string[];
-  allowSound: boolean;
-  allowVideo: boolean;
-  allowImage: boolean;
-};
+/**
+ * The config class
+ * Singleton
+ * @note The config is saved in a file called settings.json
+ */
+export class Config {
+  numberOfScreens: number = 1;
+  dataRange: DateRange = {
+    start: new Date(2020, 0, 1),
+    end: new Date(),
+  };
+  forbiddenWords: string[] = [];
+  whitelistAuthors: string[] = [];
+  whitelistHashtags: string[] = [];
+  allowSound: boolean = true;
+  allowVideo: boolean = false;
+  allowImage: boolean = true;
+
+  private static instance: Config;
+
+  private constructor() {
+    this.readConfigFromFile();
+  }
+
+  public static getInstance(): Config {
+    if (!Config.instance) {
+      Config.instance = new Config();
+    }
+    return Config.instance;
+  }
+
+  /**
+   * Writes the config to a file
+   * @param config The config to write
+   * @param fileName The name of the file to write to (default: settings.json)
+   * @note overwrites the file if it already exists
+   */
+  public writeConfigToFile(config: Config, fileName: string = 'settings.json'): void {
+    let contentToWrite: any = JSON.stringify(config, null, ' ');
+    writeFileSync(join(__dirname, fileName), contentToWrite, {
+      flag: 'w',
+    });
+  }
+
+  /**
+   * Reads the config from a file and save it to the config object
+   * @param fileName The name of the file to read from (default: settings.json)
+   */
+  public readConfigFromFile(fileName: string = 'settings.json'): void {
+    try {
+      const content = readFileSync(join(__dirname, fileName), 'utf8');
+      const config = JSON.parse(content);
+      this.numberOfScreens = config.numberOfScreens;
+      this.dataRange = config.dataRange;
+      this.forbiddenWords = config.forbiddenWords;
+      this.whitelistAuthors = config.whitelistAuthors;
+      this.whitelistHashtags = config.whitelistHashtags;
+      this.allowSound = config.allowSound;
+      this.allowVideo = config.allowVideo;
+      this.allowImage = config.allowImage;
+    } catch (e) {
+      console.error('Error while reading config file: ', e);
+    }
+  }
+}
 
 export type DateRange = {
   start: Date;
   end: Date;
 };
-
-export const defaultConfig: Config = {
-  numberOfScreens: 1,
-  dataRange: {
-    start: new Date(2020, 0, 1),
-    end: new Date(),
-  },
-  forbiddenWords: [],
-  whitelistAuthors: [],
-  whitelistHashtags: [],
-  allowSound: true,
-  allowImage: true,
-  allowVideo: false,
-};
-
-/**
- * Writes the config to a file
- * @param config The config to write
- * @param fileName The name of the file to write to (default: settings.json)
- * @note overwrites the file if it already exists
- */
-export function writeConfigToFile(config: Config, fileName: string = 'settings.json'): void {
-  let contentToWrite: any = JSON.stringify(config, null, ' ');
-  writeFileSync(join(__dirname, fileName), contentToWrite, {
-    flag: 'w',
-  });
-}
-
-/**
- * Reads the config from a file
- * @param fileName The name of the file to read from (default: settings.json)
- * @returns The config from the file
- * @note If the file does not exist, a default config is returned
- */
-export function readConfigFromFile(fileName: string = 'settings.json'): Config {
-  try {
-    const content = readFileSync(join(__dirname, fileName), 'utf8');
-    return JSON.parse(content);
-  } catch (e) {
-    console.log('No config file found, using default config');
-    return defaultConfig;
-  }
-}
