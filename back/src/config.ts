2653a311d4b42a245c4913faf45e5a76db99dae5
CodyAdam <cody.ad4@gmail.com>
2022-12-27 19:51:52 +0100

    feat: add getters

--- back/src/config.ts
+++ back/src/config.ts
@@ -7,17 +7,17 @@ import { readFileSync, writeFileSync } from 'fs';
  * @note The config is saved in a file called settings.json
  */
 export class Config {
-  numberOfScreens: number = 1;
-  dataRange: DateRange = {
+  private _numberOfScreens: number = 1;
+  private _dataRange: DateRange = {
     start: new Date(2020, 0, 1),
     end: new Date(),
   };
-  forbiddenWords: string[] = [];
-  whitelistAuthors: string[] = [];
-  whitelistHashtags: string[] = [];
-  allowSound: boolean = true;
-  allowVideo: boolean = false;
-  allowImage: boolean = true;
+  private _forbiddenWords: string[] = [];
+  private _whitelistAuthors: string[] = [];
+  private _whitelistHashtags: string[] = [];
+  private _allowSound: boolean = true;
+  private _allowVideo: boolean = false;
+  private _allowImage: boolean = true;
 
   private static instance: Config;
 
@@ -38,8 +38,8 @@ export class Config {
    * @param fileName The name of the file to write to (default: settings.json)
    * @note overwrites the file if it already exists
    */
-  public writeConfigToFile(config: Config, fileName: string = 'settings.json'): void {
-    let contentToWrite: any = JSON.stringify(config, null, ' ');
+  public writeConfigToFile(fileName: string = 'settings.json'): void {
+    let contentToWrite: any = JSON.stringify(this, null, ' ');
     writeFileSync(join(__dirname, fileName), contentToWrite, {
       flag: 'w',
     });
@@ -53,17 +53,58 @@ export class Config {
     try {
       const content = readFileSync(join(__dirname, fileName), 'utf8');
       const config = JSON.parse(content);
-      this.numberOfScreens = config.numberOfScreens;
-      this.dataRange = config.dataRange;
-      this.forbiddenWords = config.forbiddenWords;
-      this.whitelistAuthors = config.whitelistAuthors;
-      this.whitelistHashtags = config.whitelistHashtags;
-      this.allowSound = config.allowSound;
-      this.allowVideo = config.allowVideo;
-      this.allowImage = config.allowImage;
+      this._numberOfScreens = config.numberOfScreens;
+      this._dataRange = config.dataRange;
+      this._forbiddenWords = config.forbiddenWords;
+      this._whitelistAuthors = config.whitelistAuthors;
+      this._whitelistHashtags = config.whitelistHashtags;
+      this._allowSound = config.allowSound;
+      this._allowVideo = config.allowVideo;
+      this._allowImage = config.allowImage;
     } catch (e) {
-      console.error('Error while reading config file: ', e);
+      console.log('No config file found. Using default config.');
     }
+  }
+
+  public set(configObject: Partial<Config>): void {
+    this._numberOfScreens = configObject.numberOfScreens ?? this.numberOfScreens;
+    this._dataRange = configObject.dataRange ?? this.dataRange;
+    this._forbiddenWords = configObject.forbiddenWords ?? this.forbiddenWords;
+    this._whitelistAuthors = configObject.whitelistAuthors ?? this.whitelistAuthors;
+    this._whitelistHashtags = configObject.whitelistHashtags ?? this.whitelistHashtags;
+    this._allowSound = configObject.allowSound ?? this.allowSound;
+    this._allowVideo = configObject.allowVideo ?? this.allowVideo;
+    this._allowImage = configObject.allowImage ?? this.allowImage;
+  }
+
+  public get dataRange(): DateRange {
+    return this._dataRange;
+  }
+  public get numberOfScreens(): number {
+    return this._numberOfScreens;
+  }
+  public get forbiddenWords(): string[] {
+    return this._forbiddenWords;
+  }
+
+  public get whitelistAuthors(): string[] {
+    return this._whitelistAuthors;
+  }
+
+  public get whitelistHashtags(): string[] {
+    return this._whitelistHashtags;
+  }
+
+  public get allowSound(): boolean {
+    return this._allowSound;
+  }
+
+  public get allowVideo(): boolean {
+    return this._allowVideo;
+  }
+
+  public get allowImage(): boolean {
+    return this._allowImage;
   }
 }
 
