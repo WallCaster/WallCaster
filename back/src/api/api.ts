f5a956a9877adb05b3ad8d0adfe91ab8f3003406
CodyAdam <cody.ad4@gmail.com>
2023-01-31 11:35:15 +0100

    feat: new config structure

--- back/src/api/api.ts
+++ back/src/api/api.ts
@@ -3,11 +3,9 @@ import { SocialNetworkType, Post, WithId } from '../post';
 
 export abstract class Api {
   protected socialNetwork: SocialNetworkType;
-  protected config: Config;
 
   constructor(socialNetwork: SocialNetworkType) {
     this.socialNetwork = socialNetwork;
-    this.config = Config.getInstance();
   }
 
   public abstract fetchPost(): WithId<Post>;
