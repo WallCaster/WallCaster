f3907c1d8817205b4a8ef6175e629f3499089a8d
Bastien Faisant <bastien.faisant@etudiant.univ-rennes1.fr>
2023-02-05 16:38:49 +0100

    Add Dockerfile for filter-processor

---
+++ filter-processor/src/app.py
@@ -0,0 +1,19 @@
+# import os
+
+# from flask import Flask
+# app = Flask(__name__)
+
+# import filter
+
+# Import flask module
+from flask import Flask
+ 
+app = Flask(__name__)
+ 
+@app.route('/')
+def index():
+    return 'Hello to Flask!'
+ 
+# main driver function
+if __name__ == "__main__":
+    app.run()
