a85b62671f6bb9b5223d8ee4111946393b704f1c
Kali-ki <kilian.cornec@protonmail.com>
2023-02-07 00:39:42 +0100

    Avancement api-twitter classe

--- back/src/api/api-twitter.ts
+++ back/src/api/api-twitter.ts
@@ -9,58 +9,110 @@ export class ApiTwitter extends Api{
 
     public fetchPosts(): WithId<Post>[] {
 
+        // Hashtags
+        //let hashtags : string[] = this.config.whitelistHashtags;
+        let hashtags : string[] = ["figaro", "domingo", "rennes"];
+
+        // Result
+        let result : WithId<Post>[] = [];
+
         // Base url
         let url : string = "https://api.twitter.com/2/tweets/search/recent?query=";
 
         // Max results
         let max_results : number = 10;
 
-        // Values of search
+        // Langue of search
         let lang : string = "fr";
-        let hastag : string = "Lenine";
+
+        // Hashtag search
+        let search : string = "";
+        for(let i = 0; i < hashtags.length-1; i++){
+            search += "#"+hashtags[i]+" OR ";
+        }
+        search += "#"+hashtags[hashtags.length-1];
 
         // Build search part
-        let search = "#"+hastag + " lang:" + lang;
+        search = search + " lang:" + lang;
 
         // Build url
-        url = url + encodeURIComponent(search) + "&max_results=" + max_results;
-
-        console.log(url);
+        url = url + encodeURIComponent(search) +
+            "&max_results=" + max_results + // Max results
+            "&expansions=referenced_tweets.id,author_id" // Expansions -> referenced tweets, author id
+            + "&tweet.fields=created_at" // Tweet fields -> creation date
+            + "&user.fields=name,username,profile_image_url"; // User fields -> name, username, profile image
 
         // Bearer token
         let BearerToken = "AAAAAAAAAAAAAAAAAAAAAJEylgEAAAAAxkdNxil3XDR%2FtKLvaBb71e%2FA7q8%3Du2crw3ChPNn3SiIG4ZCVgXnaqfSW58vNtQYdmGsJD6hBhBtyG2";
 
+        // Import https -> https://nodejs.org/api/https.html
         const https = require('https');
 
         // Set the headers
         const options = {
             headers: {
-                'Authorization': 'Bearer '+BearerToken
+                'Authorization': 'Bearer ' + BearerToken
             }
         };
 
         // Start the request
         https.get(url, options, (res : any) => {
-
-            console.log('headers:', res.headers);
           
-            res.on('data', (d : any) => {
-              process.stdout.write(d);
+            res.on('data', (data : any) => {
+
+                process.stdout.write(data);
+
+                // Get the tweets
+                let tweet = JSON.parse(data)["includes"]["tweets"];
+
+                // Get the users
+                let user = JSON.parse(data)["includes"]["users"];
+
+                for(let i = 0; i < tweet.length; i++){
+
+                    let id_tweet = tweet[i]["id"]; // Get the id
+                    let text = tweet[i]["text"]; // Get the text
+                    let id_author = tweet[i]["author_id"]; // Get the author id
+                    let date = tweet[i]["created_at"]; // Get the date
+                    let username = "";
+                    let name = "";
+                    let image_url = "";
+
+                    for(let i = 0; i < user.length; i++){
+                        if(user[i]["id"] == id_author){
+                            name = user[i]["name"];
+                            username = user[i]["username"];
+                            image_url = user[i]["profile_image_url"];
+                        }
+                    }
+
+                    console.log("id_tweet : " + id_tweet);
+                    console.log("text : " + text);
+                    console.log("id_author : " + id_author);
+                    console.log("date : " + date);
+                    console.log("username : " + username);
+                    console.log("name : " + name);
+                    console.log("image_url : " + image_url);
+
+                    // Push the post on the result array
+                    result.push(postWithId({
+                        content:{text : text},
+                        author:{name : name, username : username, image : image_url},
+                        date: new Date(date),
+                        originUrl:"https://twitter.com/"+username+"/status/"+id_tweet,
+                        socialNetwork: this.socialNetwork
+                    }));
+
+                }
+
             });
 
-        }).on('error', (e : any) => {
-            console.error(e);
         });
 
-        return [];
+        // Display the result
+        console.log(result.length);
 
-        /*return postWithId({
-            content:{text : ""},
-            author:{name : "", username : ""},
-            date: new Date(Date.now()),
-            originUrl:"",
-            socialNetwork: this.socialNetwork
-        });*/
+        return result;
 
     }
     
