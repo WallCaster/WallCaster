0f0540541fe11452397a969d627287b95e7a08b8
CodyAdam <cody.ad4@gmail.com>
2023-02-27 14:56:20 +0100

    fix: animation cahnge delay

--- front-client/src/components/App.tsx
+++ front-client/src/components/App.tsx
@@ -1,5 +1,5 @@
 import { Transition } from '@headlessui/react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useTimeoutFn } from 'react-use';
 import useSocket from '../../hooks/useSocket';
 import type { Post } from '../types/post';
@@ -20,16 +20,28 @@ export const App = () => {
     originUrl: 'https://twitter.com/johndoe/status/1',
   };
   const [post, setPost] = useState<Post | null>(null);
+  const [nextPost, setNextPost] = useState<Post | null>(null);
   const [serverIp, setServerIp] = useState('http://localhost:3001');
   let [isShowing, setIsShowing] = useState(true);
   let [, , resetIsShowing] = useTimeoutFn(() => setIsShowing(true), 400);
   const socket = useSocket(serverIp, (socket) => {
     socket.on('post', (p) => {
-      setPost(p);
-      setIsShowing(false);
-      resetIsShowing();
+      updatePost(p);
     });
   });
+
+  function updatePost(p: Post) {
+    setNextPost(p);
+    setIsShowing(false);
+    resetIsShowing();
+  }
+
+  useEffect(() => {
+    if (!post && nextPost) {
+      setPost(nextPost);
+      setNextPost(null);
+    }
+  }, [post, nextPost]);
 
   if (socket == null) {
     return (
@@ -40,7 +52,7 @@ export const App = () => {
           placeholder='Type here'
           value={serverIp}
           onChange={(e) => setServerIp(e.target.value)}
-          className='w-full max-w-xs bg-black/5 p-2 border-b-4 border-b-white/40 text-sm'
+          className='w-full max-w-xs bg-black/10 p-2 border-b-4 border-b-white/50 text-sm'
         />
       </div>
     );
@@ -51,6 +63,7 @@ export const App = () => {
       <div className='h-full px-16 py-14 flex items-center justify-center'>
         {post && (
           <Transition
+            appear
             show={isShowing}
             enter='transform transition duration-[500ms]'
             enterFrom='opacity-0 rotate-[40deg] scale-50 translate-x-[50vw] -translate-y-32'
@@ -58,6 +71,10 @@ export const App = () => {
             leave='transform duration-[300ms] transition ease-[cubic-bezier(.6,-0.46,.63,.75)]'
             leaveFrom='opacity-100 rotate-0 scale-100 '
             leaveTo='opacity-0 rotate-[-40deg] scale-50 -translate-x-[50vw] -translate-y-32'
+            afterLeave={() => {
+              setPost(nextPost);
+              setNextPost(null);
+            }}
           >
             <PostCard post={post} className='rounded-3xl shadow-2xl' />
           </Transition>
