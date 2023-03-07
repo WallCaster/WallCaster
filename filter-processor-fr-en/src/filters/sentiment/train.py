73379999269cbb4501da5053f14d339ca5019f40
Angora Kouame <kouame.angora@etudiant.univ-rennes1.fr>
2023-03-07 13:14:43 +0100

    données entrainee sur le dataset fr-en

--- filter-processor-fr-en/src/filters/sentiment/train.py
+++ filter-processor-fr-en/src/filters/sentiment/train.py
@@ -1,88 +1,112 @@
-from pathlib import Path
-from nltk.stem.wordnet import WordNetLemmatizer
-from nltk.corpus import twitter_samples, stopwords
-from nltk.tag import pos_tag
-from nltk import FreqDist, NaiveBayesClassifier
-import re, string, random
+import re
+import spacy
 import pickle
+import pandas as pd
+from pathlib import Path
 
-# this function will remove noise from the tweets such as hyperlinks, mentions, 
-# and special characters using regular expressions and lemmatize the words
-def remove_noise(tweet_tokens, stop_words = ()):
+from nltk.corpus import stopwords
+from nltk.tokenize import punkt
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
 
-    cleaned_tokens = []
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
 
-    for token, tag in pos_tag(tweet_tokens):
-        token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|'\
-                       '(?:%[0-9a-fA-F][0-9a-fA-F]))+','', token)
-        token = re.sub("(@[A-Za-z0-9_]+)","", token)
+# this function is used to create a dataframe from the data from 2 files csv , one in english and one in french
+def create_dataframe():
+    df_tweets_fr = pd.read_csv('../data/french_tweets.csv', sep=',', encoding='utf-8')
+    df_tweets_en = pd.read_csv('../data/english_tweets.csv', sep=',', encoding='ISO-8859-1')
 
-        if tag.startswith("NN"):
-            pos = 'n'
-        elif tag.startswith('VB'):
-            pos = 'v'
-        else:
-            pos = 'a'
+    # keep only 50 tweets randomly for each language
+    df_tweets_fr = df_tweets_fr.sample(n=50, random_state=1)
+    df_tweets_en = df_tweets_en.sample(n=50, random_state=1)
 
-        lemmatizer = WordNetLemmatizer()
-        token = lemmatizer.lemmatize(token, pos)
+    # rename columns df_tweets_en ['label', 'id', 'Date', 'Query', 'Username', 'tweet']
+    df_tweets_en = df_tweets_en.rename(columns={
+        '0': 'label',
+        '1467810369': 'id',
+        'Mon Apr 06 22:19:45 PDT 2009': 'Date',
+        'NO_QUERY': 'Query',
+        '_TheSpecialOne_': 'Username',
+        "@switchfoot http://twitpic.com/2y1zl - Awww, that's a bummer.  You shoulda got David Carr of Third Day to do it. ;D": 'tweet'
+    })
 
-        if len(token) > 0 and token not in string.punctuation and token.lower() not in stop_words:
-            cleaned_tokens.append(token.lower())
-    return cleaned_tokens
- 
-def get_all_words(cleaned_tokens_list):
-    for tokens in cleaned_tokens_list:
-        for token in tokens:
-            yield token
+    # rename columns df_tweets_fr ['label', 'text']
+    df_tweets_fr = df_tweets_fr.rename(columns={
+        'text': 'tweet',
+    })
 
-def get_tweets_for_model(cleaned_tokens_list):
-    for tweet_tokens in cleaned_tokens_list:
-        yield dict([token, True] for token in tweet_tokens)
+    # add column 'language' to df_tweets_en and df_tweets_fr
+    df_tweets_en['lang'] = 'en'
+    df_tweets_fr['lang'] = 'fr'
 
-if __name__ == "__main__":
+    # fill 'en' in column 'language' of df_tweets_en and 'fr' in column 'language' of df_tweets_fr
+    df_tweets_en['lang'] = df_tweets_en['lang'].fillna('en')
+    df_tweets_fr['lang'] = df_tweets_fr['lang'].fillna('fr')
 
-    positive_tweets = twitter_samples.strings('positive_tweets.json')
-    negative_tweets = twitter_samples.strings('negative_tweets.json')
-    text = twitter_samples.strings('tweets.20150430-223406.json')
-    tweet_tokens = twitter_samples.tokenized('positive_tweets.json')[0]
+    # create a new dataframe with the tweets and the labels
+    df_tweet = pd.concat([df_tweets_fr[['label', 'tweet','lang']], df_tweets_en[['label', 'tweet', 'lang']]], ignore_index=True, axis=0)
 
-    stop_words = stopwords.words('english')
+    # remplace 4 by 1
+    df_tweet['label'] = df_tweet['label'].replace(4, 1)
 
-    positive_tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
-    negative_tweet_tokens = twitter_samples.tokenized('negative_tweets.json')
+    # split the data into train and test
+    train_data, test_data = train_test_split(df_tweet, test_size=0.2, random_state=42)
 
-    positive_cleaned_tokens_list = []
-    negative_cleaned_tokens_list = []
+    return train_data, test_data
 
-    for tokens in positive_tweet_tokens:
-        positive_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
+# this function is used to preprocess the data (remove stopwords, punctuation, etc.)
+def preprocess_tweet(text, lang):
+    # Supprimer les mentions, les hashtags et les liens
+    text = re.sub(r'@\w+|#\w+|https?://\S+', '', text)
+    # Supprimer les caractères spéciaux
+    text = re.sub(r'[^\w\s]', '', text)
+    # Convertir en minuscules
+    text = text.lower()
+    # Tokenisation
+    if lang == 'fr':
+        tokens = word_tokenize(text, language='french')
+    elif lang == 'en':
+        tokens = word_tokenize(text, language='english')
+    # Supprimer les stopwords
+    stop_words = set(stopwords.words(lang))
+    filtered_tokens = [token for token in tokens if token not in stop_words]
+    # Stemming ou lemmatisation
+    if lang == 'fr':
+        stemmer = SnowballStemmer('french')
+        preprocessed_text = ' '.join([stemmer.stem(token) for token in filtered_tokens])
+    elif lang == 'en':
+        nlp = spacy.load('en_core_web_sm')
+        preprocessed_text = ' '.join([token.lemma_ for token in nlp(' '.join(filtered_tokens))])
 
-    for tokens in negative_tweet_tokens:
-        negative_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
+    return preprocessed_text
 
-    all_pos_words = get_all_words(positive_cleaned_tokens_list)
+if __name__ == '__main__':
+    train_data, test_data = create_dataframe()
+    # Prétraitement des données d'entraînement
+    preprocessed_train_data = train_data.apply(lambda x: preprocess_tweet(x['tweet'], 'en' if x['lang'] == 'en' else 'fr'), axis=1)
+    train_labels = train_data['label']
 
-    freq_dist_pos = FreqDist(all_pos_words)
-    #print(freq_dist_pos.most_common(10))
+    # Prétraitement des données de test
+    preprocessed_test_data = test_data.apply(lambda x: preprocess_tweet(x['tweet'], 'en' if x['lang'] == 'en' else 'fr'), axis=1)
+    test_labels = test_data['label']
 
-    positive_tokens_for_model = get_tweets_for_model(positive_cleaned_tokens_list)
-    negative_tokens_for_model = get_tweets_for_model(negative_cleaned_tokens_list)
+    # tranformer les tweets prétraités en vecteurs de fonctionnalités numériques qui peuvent être utilisés par les algorithmes d'apprentissage automatique
+    vectorizer = TfidfVectorizer()
+    train_features = vectorizer.fit_transform(preprocessed_train_data)
+    test_features = vectorizer.transform(preprocessed_test_data)
 
-    positive_dataset = [(tweet_dict, "Positive")
-                         for tweet_dict in positive_tokens_for_model]
+    # entraîner un modèle de classification
+    classifier = LogisticRegression()
+    classifier.fit(train_features, train_labels)
 
-    negative_dataset = [(tweet_dict, "Negative")
-                         for tweet_dict in negative_tokens_for_model]
-
-    dataset = positive_dataset + negative_dataset
-
-    random.shuffle(dataset)
-
-    train_data = dataset[:7000]
-    test_data = dataset[7000:]
-
-    classifier = NaiveBayesClassifier.train(train_data)
+    # évaluer le modèle
+    predictions = classifier.predict(test_features)
+    print('Accuracy: ', accuracy_score(test_labels, predictions))
+    print('Classification report: ', classification_report(test_labels, predictions))
 
     # save the classifier
     path = Path(__file__).parent / "trained_data.pickle"
