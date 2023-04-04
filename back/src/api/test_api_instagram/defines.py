743755677a4e27b3b53fa1139d821868b1db425a
Louis-Gabriel CAPLIEZ <odd57@free.fr>
2023-04-04 17:48:24 +0200

    First try of using the Instagram API. I don't think this will be doable due to missing permissions. Bug to fix in get_long_lived_access_token.py.

---
+++ back/src/api/test_api_instagram/defines.py
@@ -0,0 +1,40 @@
+import requests
+import json
+
+def getCreds():
+
+    creds = dict()
+
+    creds['access_token'] = 'EAAUXdXXwOr0BAHogfgTtV5zMYHMhUJLWmEuUYdnf4lYcp5tvk0LRCexrU9jF1ySPl3MdrL2ZCdTAZCALxgvf0SnaiCwZBLGx4AchQjyiMyPkAZBBtsgCrLhl5NZBDkdEyYtUt8LeyL2QGbtAIMklH2PHdwV3kfxulMNgNGsmmxDCm2ZAc48nwZC54xZApecW4BimcbuHs8Qbcb3pBciEDZBSyw8R0qtonofgZD'
+    creds['client_id'] = '1433168140843709'
+    creds['client_secret'] = '3adaa1565c11f23e46bbc3a3c328b21a'
+    creds['graph_domain'] = 'https://graph.facebook.com/'
+    creds['graph_version'] = 'v16.0'
+    creds['endpoint_base'] = creds['graph_domain'] + creds['graph_version'] + '/'
+    creds['debug'] = 'no'
+
+    return creds
+
+def makeApiCall(url, endPointParams, debug = 'no'):
+
+    data = requests.get(url, endPointParams)
+    response = dict()
+    response['url'] = url
+    response['endpoint_params'] = endPointParams
+    response['endpoint_params_pretty'] = json.dumps(endPointParams, indent = 4)
+    response['json_data'] = json.loads(data.content)
+    response['json_data_pretty'] = json.dumps(response['json_data'], indent = 4)
+
+    if (debug == 'yes'):
+        displayApiCallData(response)
+
+    return response
+
+def displayApiCallData(response):
+
+    print("\nURL: ")
+    print(response["url"])
+    print("\nEndpoint Params: ")
+    print(response['endpoint_params_pretty'])
+    print("\nResponse: ")
+    print(response['json_data_pretty'])
