5bad18334427ae330657c36f4d5b1f3bc63afcce
CodyAdam <cody.ad4@gmail.com>
2023-04-04 17:13:20 +0200

    clean filter

--- filter-processor/src/filters/sentiment/run.py
+++ filter-processor/src/filters/sentiment/run.py
@@ -1,56 +1,23 @@
-from nltk.tokenize import word_tokenize
-from pathlib import Path
 import pickle
 from pathlib import Path
-from nltk.stem.wordnet import WordNetLemmatizer
-from nltk.tag import pos_tag
-import re, string
-import pickle
 
+# charger le classificateur
 path = Path(__file__).parent / "trained_data.pickle"
 with path.open("rb") as file:
     classifier = pickle.load(file)
 
-
-# this function will remove noise from the tweets such as hyperlinks, mentions,
-# and special characters using regular expressions and lemmatize the words
-def remove_noise(tweet_tokens, stop_words=()):
-
-    cleaned_tokens = []
-
-    for token, tag in pos_tag(tweet_tokens):
-        token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|'\
-                       '(?:%[0-9a-fA-F][0-9a-fA-F]))+','', token)
-        token = re.sub("(@[A-Za-z0-9_]+)", "", token)
-
-        if tag.startswith("NN"):
-            pos = 'n'
-        elif tag.startswith('VB'):
-            pos = 'v'
-        else:
-            pos = 'a'
-
-        lemmatizer = WordNetLemmatizer()
-        token = lemmatizer.lemmatize(token, pos)
-
-        if len(token) > 0 and token not in string.punctuation and token.lower(
-        ) not in stop_words:
-            cleaned_tokens.append(token.lower())
-    return cleaned_tokens
-
-
 def is_positive(txt: str) -> bool:
-    tokens = remove_noise(word_tokenize(txt))
-    res = classifier.classify(dict([token, True] for token in tokens))
-    if res == "Positive":
+    res = classifier(txt)
+    if res[0]['label'] == "positive":
         return True
     else:
         return False
-
-
+    
 if __name__ == "__main__":
     print("Ready to process sentences. (Ctrl+C to exit)\n")
 
     while True:
         txt = input("Enter a sentence: ")
         print("The result is: ", is_positive(txt), end="\n\n")
+
+
